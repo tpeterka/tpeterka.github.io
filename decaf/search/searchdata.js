fdaf24e5ebc5b7affdf95bd32ea6468c0905b087
Orcun YILDIZ <orcun@mcswl098.mcs.anl.gov>
2018-06-20 13:02:55 -0500

    adding again the doxygen html files

---
+++ decaf/search/searchdata.js
@@ -0,0 +1,24 @@
+var indexSectionsWithContent =
+{
+  0: "abcdfghilmnoprstuvwz",
+  1: "abcdfmoprsvw",
+  2: "abcdfghimnprstuvw",
+  3: "abcdfilmnoprstwz"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "functions",
+  3: "variables"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Functions",
+  3: "Variables"
+};
+
